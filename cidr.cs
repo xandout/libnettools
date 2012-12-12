a780a93501b3e6bf9258d213cb50c0fa5105a442
xandout <turnerdistributed@gmail.com>
2012-12-12 11:13:15 -0500

    Update cidr.cs

--- cidr.cs
+++ cidr.cs
@@ -27,5 +27,12 @@ namespace libnettools
             network[0] = "Not Implemented";
             return network;
         }
+        
+        public static string[] getGateway(string ipaddr)
+        {
+            string[] network = new string[1];
+            network[0] = "Not Implemented";
+            return network;
+        }
     }
 }
